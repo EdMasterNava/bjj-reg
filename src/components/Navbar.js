341336e9c77a7c718a393bb354ca7b19c7657517
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-01 19:13:30 -0800

    Added Font Awesome Library

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useMatch, useResolvedPath} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
 import "./css/Navbar.css";
 function Navbar(){
     //state
-
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     //render
     return (
         <nav className="nav">
-            <Link to="/bjj-reg/" className="site-title">BJJ Register</Link>
+            <div className="left-nav">
+                <FontAwesomeIcon icon={click ? faXmark : faBars} onClick={handleClick}/>
+                <Link to="/bjj-reg/" className="site-title">BJJ Register</Link>
+            </div>
             <ul>
                 <CustomLink to="/bjj-reg/events">Events</CustomLink>
             </ul>
