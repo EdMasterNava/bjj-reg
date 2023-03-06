dabf86c3afe1858833af7e8c7bbaba508160a428
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-05 17:27:00 -0800

    Added Register and Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './css/Login.css';
 
 function Login(){
-    return <div className='placeholder'>Login</div>
+    return (
+        <div className='register-container'>
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input type='email' placeholder='your-email@example.com' id='email' name='email'/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type='password' placeholder='********' id='password' name='password'/>
+                </div>
+                <button type='submit'>Log In</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './css/Register.css';
 
 function Register(){
-    return <div className='placeholder'>Register</div>
+    return (
+        <div className='register-container'>
+                <form>
+                    <div>
+                        <label>Name</label>
+                        <input type='text' placeholder='John Doe' id='name' name='name'/> 
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input required type='email' placeholder='your-email@example.com' id='email' name='email'/>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input required type='password' placeholder='********' id='password' name='password'/>
+                    </div>
+                    <div>
+                        <label>Confirm Password</label>
+                        <input required type='password' placeholder='********' id='password-confirmation' name='password-confirmation'/>
+                    </div>
+                    <button type='submit' >Register</button>
+                </form>
+        </div>
+    )
 }
 
 export default Register;
