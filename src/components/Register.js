954fe9ed11b2a80e193834523bb356899cac1b7d
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-08 15:12:26 -0800

    Added Firebase to Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,77 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useAuth } from './AuthContext';
+import { Link } from 'react-router-dom';
 import './css/Register.css';
 
 function Register(){
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { register, currentUser } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        if(passwordRef.current.value !== passwordConfirmRef.current.value){
+            return setError('Password Do Not Match');
+        }
+        try{
+            setError('');
+            setLoading(true);
+            await register(emailRef.current.value, passwordRef.current.value); 
+        } catch {
+            return setError('Failed To Create An Account');
+        }
+        
+    }
+
     return (
         <div className='register-container'>
-                <form>
+                <form onSubmit={handleSubmit}>
+                    <div className='form-title'>Register</div>
+                    {error ? <div className='error'>{error}</div> : <div></div>}
+                    {currentUser && currentUser.email}
                     <div>
                         <label>Name</label>
-                        <input type='text' placeholder='John Doe' id='name' name='name'/> 
+                        <input  type='text' 
+                                placeholder='John Doe' 
+                                id='name' 
+                                name='name'
+                                ref={nameRef}/> 
                     </div>
                     <div>
                         <label>Email</label>
-                        <input required type='email' placeholder='your-email@example.com' id='email' name='email'/>
+                        <input  required 
+                                type='email' 
+                                placeholder='your-email@example.com' 
+                                id='email' 
+                                name='email'
+                                ref={emailRef}/>
                     </div>
                     <div>
                         <label>Password</label>
-                        <input required type='password' placeholder='********' id='password' name='password'/>
+                        <input  required 
+                                type='password' 
+                                placeholder='********' 
+                                id='password' 
+                                name='password'
+                                ref={passwordRef}/>
                     </div>
                     <div>
                         <label>Confirm Password</label>
-                        <input required type='password' placeholder='********' id='password-confirmation' name='password-confirmation'/>
+                        <input  required 
+                                type='password' 
+                                placeholder='********' 
+                                id='password-confirmation' 
+                                name='password-confirmation'
+                                ref={passwordConfirmRef}/>
                     </div>
-                    <button type='submit' >Register</button>
+                    <button type='submit' disabled={loading}>Register</button>
                 </form>
                 <div className='form-footer'>
-                    Already have an account? Sign In
+                    Already have an account? <Link to="/bjj-reg/login">Sign In</Link>
                 </div>
         </div>
     )
