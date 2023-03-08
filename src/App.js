e1e9e60736f7615e18debd5645e132da15b04926
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-08 15:10:33 -0800

    Added AuthProvider to App

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Home from "./components/Home";
 import Register from './components/Register';
 import Login from "./components/Login";
 import "./components/css/App.css";
+import { AuthProvider } from "./components/AuthContext";
 function App(){
     return (
       <>
-        <Navbar />
-        <div className="main-container">
-          <Routes>
-            <Route path="/bjj-reg/" element={<Home />} />
-            <Route path="/bjj-reg/events" element={<Events />} />
-            <Route path="/bjj-reg/register" element={<Register />} />
-            <Route path="/bjj-reg/login" element={<Login />} />
-          </Routes>
-        </div>
+        <AuthProvider>
+          <Navbar />
+          <div className="main-container">
+            <Routes>
+              <Route path="/bjj-reg/" element={<Home />} />
+              <Route path="/bjj-reg/events" element={<Events />} />
+              <Route path="/bjj-reg/register" element={<Register />} />
+              <Route path="/bjj-reg/login" element={<Login />} />
+            </Routes>
+          </div>
+        </AuthProvider>
       </>
     )
 }
