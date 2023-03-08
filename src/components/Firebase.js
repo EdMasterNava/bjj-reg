94f5bf0e070ebe0d1fa97999d725da57efb3dcb5
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-08 15:09:34 -0800

    Updated Firebase Keys

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+const app = initializeApp({
+    apiKey: "AIzaSyAgb5xmZfyGfQ1X4FiOPjOL3dBh0WZERkg",
+    authDomain: "bjj-register-dev.firebaseapp.com",
+    projectId: "bjj-register-dev",
+    storageBucket: "bjj-register-dev.appspot.com",
+    messagingSenderId: "197026041821",
+    appId: "1:197026041821:web:cf2707e70978225d2c7034"
 });
 
-export const auth = app.auth();
+export const auth = getAuth(app);
 export default app;
